01cd57255bbab4cee47186e2a70f8157f6241273
swade781977 <swade781977@yahoo.com>
2020-12-13 15:33:46 -0500

    fixed parameters for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
     def set_user
         @user = current_user
     end
 
     protected
 
-    def configure_permitted_perameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :last_name, :addres, :city, :state, :zip, :phone_number, :employee, :manager])
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :last_name, :address, :city, :state, :zip, :phone_number, :employee, :manager])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:email, :first_name, :last_name, :address, :city, :state, :zip, :phone_number, :employee, :manager])
     end
 end
